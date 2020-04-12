fa72e5e956a59c448e67ce1dba20253c2cdb24b8
Ed Chebli <chah762002@yahoo.fr>
2020-04-12 15:36:35 -0700

    Set the server up and added the basic files

---
+++ app.js
@@ -0,0 +1,29 @@
+// Requiring Modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Connect Mongoose to the DB
+mongoose.connect("mongodb://localhost:27017/dz_eShop", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("err", console.error.bind(console, "Connection error"));
+db.once("open", () => console.log("Successfully connected to DB"));
+
+// App Config
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Listening to the server on port 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port port 🔥`));
